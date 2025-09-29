53b75901b4ff89a3300ef665cc81b8c53e90a436
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-09-29 07:18:11 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,31 +1,37 @@
 import React from "react";
-import './../styles/App.css'; 
-function Menu(props) {
-  const menuItems = props.menuItems;
-  const handleSort = props.handleSort;
+import "./../styles/App.css";
 
+function Menu({ menuItems, handleSort }) {
   return (
     <div>
       <h1 className="heading">Our Menu</h1>
       <nav className="nav-bar">
-        <a onClick={()=>handleSort("all")}>All</a>
-        <a onClick={() => handleSort("breakfast")}>Breakfast</a>
-        <a onClick={()=>handleSort("lunch")}>Lunch</a>
-        <a onClick={()=>handleSort("shakes")}>Shakes</a>
+        <a id="filter-btn-0" onClick={() => handleSort("all")}>
+          All
+        </a>
+        <a id="filter-btn-1" onClick={() => handleSort("breakfast")}>
+          Breakfast
+        </a>
+        <a id="filter-btn-2" onClick={() => handleSort("lunch")}>
+          Lunch
+        </a>
+        <a id="filter-btn-3" onClick={() => handleSort("shakes")}>
+          Shakes
+        </a>
       </nav>
       <div className="menu-items">
-        {menuItems.map((item, index) => (
-          <div className="menu-container" key={index}>
+        {menuItems.map((item) => (
+          <div className="menu-container" key={item.id}>
             <div className="left">
-              <img src={item.image} />
+              <img src={item.image} alt={item.title} />
             </div>
             <div className="right">
               <div className="top">
                 <h3>{item.title}</h3>
-                <h5>{item.price}</h5>
+                <h5>₹{item.price}</h5>
                 <h5>{item.category}</h5>
               </div>
-                 <hr className="line"/>
+              <hr className="line" />
               <div className="bottom">
                 <p>{item.description}</p>
               </div>
@@ -36,4 +42,5 @@ function Menu(props) {
     </div>
   );
 }
+
 export default Menu;
