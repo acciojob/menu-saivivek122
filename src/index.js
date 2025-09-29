a9eabbd74ee543764a7a3af944abb083a03f4401
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-09-29 07:43:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
-
+// Attach App to the root div in public/index.html
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
